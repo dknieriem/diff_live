8f12905b9033d58a52300000fcaeff2e6e670464
Don Knieriem <don@thecanarium.com>
2024-06-07 22:07:09 -0400

    dmp helper functions

--- cmd/wasm/diff/diff_match_patch.go
+++ cmd/wasm/diff/diff_match_patch.go
@@ -84,7 +84,7 @@ func (dmp *DiffMatchPatch) diffMainDeadline(inputA, inputB string, checklines bo
 	textChoppedB = textChoppedB[:len(inputB)-commonLength]
 
 	// Compute the diff on the middle block.
-	diffs := dmp.diffCompute(textChop, inputB, checklines, deadline)
+	diffs := dmp.diffCompute(textChoppedA, textChoppedB, checklines, deadline)
 
 	// Restore the prefix and suffix.
 	if commonPrefix != "" {
@@ -172,12 +172,9 @@ func (dmp *DiffMatchPatch) diffCompute(textA, textB string, checklines bool, dea
 // * diff_lineMode_
 func (dmp *DiffMatchPatch) diff_lineMode(textA, textB string, deadline time.Time) []Diff {
 	// Scan the text on a line-by-line basis first.
-	b := dmp.diff_linesToChars(textA, textB)
-	textA = b[0].toString()
-	textB = b[1].toString()
-	lineArray := b[2].toStringList()
+	textA, textB, lineArray := dmp.diff_linesToChars(textA, textB)
 
-	diffs := dmp.diffMainDeadline(textA, textB, false, deadline)
+	_, diffs := dmp.diffMainDeadline(textA, textB, false, deadline)
 
 	// Convert the diff back to original text.
 	diffs = dmp.diff_charsToLines(diffs, lineArray)
@@ -335,16 +332,157 @@ func (dmp *DiffMatchPatch) diff_bisect_(textA, textB string, deadline time.Time)
 	return diffs
 }
 
+// * diff_bisectSplit
+func (dmp *DiffMatchPatch) diff_bisectSplit(textA, textB string, x, y int, deadline time.Time) []Diff {
+	textA1 := textA[:x]
+	textB1 := textB[:y]
+	textA2 := textA[x:]
+	textB2 := textB[y:]
+
+	// Compute both diffs serially.
+	_, diffs := dmp.diffMainDeadline(textA1, textB1, false, deadline)
+	_, diffsb := dmp.diffMainDeadline(textA2, textB2, false, deadline)
+
+	return append(diffs, diffsb...)
+}
+
+// * diff_linesToChars
+func (dmp *DiffMatchPatch) diff_linesToChars(textA, textB string) ([]uint32, []uint32, []string) {
+
+	// "\x00" is a valid character, but various debuggers don't like it.
+	// So we'll insert a junk entry to avoid generating a null character.
+	lineArray := []string{""}
+	lineHash := make(map[string]int)
+	// e.g. linearray[4] == "Hello\n"
+	// e.g. linehash.get("Hello\n") == 4
+
+	chars1 := dmp.diff_linesToCharsMunge(textA, lineArray, lineHash)
+	chars2 := dmp.diff_linesToCharsMunge(textB, lineArray, lineHash)
+
+	return chars1, chars2, lineArray
+}
+
+// * diff_linestoCharsMunge
+func (dmp *DiffMatchPatch) diff_linesToCharsMunge(text string, lineArray []string, lineHash map[string]int) []uint32 {
+	lineStart := 0
+	lineEnd := -1
+	chars := []uint32{}
+
+	// Walk the text, pulling out a substring for each line.
+	// text.split('\n') would would temporarily double our memory footprint.
+	// Modifying text would create many large strings to garbage collect.
+	for lineEnd < len(text)-1 {
+		lineEnd = strings.Index(text[lineStart:], "\n")
+		if lineEnd == -1 {
+			lineEnd = len(text) - 1
+		}
+		line := text[lineStart : lineEnd+1]
+		lineStart = lineEnd + 1
+		lineValue, ok := lineHash[line]
+
+		if ok {
+			chars = append(chars, uint32(lineValue))
+		} else {
+			lineArray = append(lineArray, line)
+			lineHash[line] = len(lineArray) - 1
+			chars = append(chars, uint32(len(lineArray)-1))
+		}
+	}
+	return chars
+}
+
+// * diff_charsToLines
+func (dmp *DiffMatchPatch) diff_charsToLines(diffs []Diff, lineArray []string) []Diff {
+	diffsWithText := make([]Diff, 0, len(diffs))
+
+	for _, diff := range diffs {
+		text := make([]string, len(diff.Text))
+
+		for y := 0; y < len(diff.Text); y++ {
+			text[y] = lineArray[uint32(diff.Text[y])]
+		}
+		diff.Text = strings.Join(text, "")
+		diffsWithText = append(diffsWithText, diff)
+	}
+
+	return diffsWithText
+}
+
 // * diff_commonPrefix
 func (dmp *DiffMatchPatch) diff_commonPrefix(textA, textB string) int {
-
+	// Performance analysis: http://neil.fraser.name/news/2007/10/09/
+	n := min(len(textA), len(textB))
+	for i := 0; i < n; i++ {
+		if textA[i] != textB[i] {
+			return i
+		}
+	}
+	return n
 }
 
 // * diff_commonSuffix
-// * diff_cleanupMerge
+func (dmp *DiffMatchPatch) diff_commonSuffix(textA, textB string) int {
+	// Performance analysis: http://neil.fraser.name/news/2007/10/09/
+	textALen := len(textA)
+	textBLen := len(textB)
+	n := min(textALen, textBLen)
+
+	for i := 1; i < n; i++ {
+		if textA[textALen-i] != textB[textBLen-i] {
+			return i - 1
+		}
+	}
+	return n
+}
+
+// * diff_commonOverlap
+func (dmp *DiffMatchPatch) diff_commonOverlap(textA, textB string) int {
+	// Cache the text lengths to prevent multiple calls.
+	textALen := len(textA)
+	textBLen := len(textB)
+	// Eliminate the null case.
+	if textALen == 0 || textBLen == 0 {
+		return 0
+	}
+	// Truncate the longer string.
+	textA_trunc := textA
+	textB_trunc := textB
+	if textALen > textBLen {
+		textA_trunc = textA[textALen-textBLen:]
+	} else if textALen < textBLen {
+		textB_trunc = textB[:textALen]
+	}
+	text_length := min(textALen, textBLen)
+	// Quick check for the worst case.
+	if textA_trunc == textB_trunc {
+		return text_length
+	}
+
+	// Start by looking for a single character match
+	// and increase length until no match is found.
+	// Performance analysis: http://neil.fraser.name/news/2010/11/04/
+	best := 0
+	length := 1
+	for {
+		pattern := textA_trunc[len(textA_trunc)-length:]
+		found := strings.Index(textB_trunc, pattern)
+		if found == -1 {
+			return best
+		}
+		length += found
+		if found == 0 || textA_trunc[len(textA_trunc)-length:] == textB_trunc[:length] {
+			best = length
+			length++
+		}
+	}
+
+	return best
+}
+
 // * diff_halfMatch
 
-// * new Diff()
+// * diff_cleanupMerge
+
 // * diff_cleanupSemantic
 // * diff_prettyHtml
 // Convert a diff array into a pretty HTML report.
